84ed9ff0c8554e7a5ba5fa6d1a182df0f8d4348a
Hans Fase <hfase01@gmail.com>
2012-09-19 17:32:31 -0400

    Base app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railslime::Application.config.secret_token = '4d4fff7fe7560b7c870315562d678e95e3c4fb421f658a3caa400024c7e0dc1a3ce5a54081ff79bef970240d9a62d882484fc604f21b6954e16a32a4c2d8b5e5'
